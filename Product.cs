aa5fe4fd3207f42ede62a9eb90f8dd8090a873e1
Aygun <sahmuradova18@gmail.com>
2024-03-28 12:33:27 +0400

    LabTask14

---
+++ Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask14
+{
+	internal class Product
+	{
+		public string Id;
+		public string BrandName;
+		public string Model;
+		public int Price;
+		public int Cost;
+		public int Income;
+		public int Count;
+		public Product(string id, string brandname, string model, int price, int cost, int count)
+		{
+			Id = id;
+			BrandName = brandname;
+			Model = model;
+			Price = price;
+			Cost = cost;
+			Count = count;
+			Sale();
+		}
+		public void GetInfo()
+		{
+			Console.WriteLine($"Id:{Id},BrandName:{BrandName},Model:{Model},Price:{Price},Cost:{Cost},Count:{Count},Income:{Income}");
+			
+		}
+		public void Sale()
+		{
+			if (Count > 0)
+			{
+				Price = Price - Cost;
+				Income += Price;
+				Income *= Count;
+				Count--;
+			}
+			else
+			{
+				Income = 0;
+			}
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace LabTask14
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Product product=new Product("A123","Bmw","bmw",3000,120,2);
+			product.GetInfo();
+        }
+	}
+}
